1f3d37a1811e175230eea908416b50fe3897e3fc
matej-mac <vanikmatej99@gmail.com>
2024-07-08 21:21:14 +0300

    implement navigation, wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,22 @@
 import { Route, BrowserRouter, Routes } from 'react-router-dom';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Col } from 'react-bootstrap';
 
-import Home from './pages/Home';
-import Nav from './components/Nav';
+import Dashboard from './pages/Dashboard';
+import Navigation from './components/Navigation';
 import Footer from './components/Footer';
 import Import from './pages/Import';
 
 export default function App() {
   return (
     <BrowserRouter>
-      <Container fluid id="MainLoyout" className="d-flex flex-row vh-100">
+      <Container fluid id="MainLoyout" className="d-flex flex-row vh-100 px-4 py-2">
         <Col xs={2} className="d-flex my-3">
-          <Nav />
+          <Navigation />
         </Col>
         <Col className='d-flex flex-column'>
           <Container className='flex-fill'>
             <Routes>
-              <Route path="/" element={<Home />} />
+              <Route path="/" element={<Dashboard />} />
               <Route path="/import" element={<Import />} />
             </Routes>
           </Container>
---
+++ src/components/Navigation.tsx
@@ -0,0 +1,42 @@
+import { Container, Navbar, Nav } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
+
+type NavigationButtonType = {
+  to: string;
+  text: string;
+  icon?: string;
+};
+
+function NavEntry(props: NavigationButtonType) {
+  const { text, icon, to } = props;
+
+  return (
+    <NavLink
+      to={to}
+      className={({ isActive }) =>
+        `d-flex gap-3 p-2 m-2 rounded-3 text-white ${
+          isActive ? 'active-nav-link' : 'inactive-nav-link'
+        }`
+      }
+    >
+      <i className={icon}></i> {text}
+    </NavLink>
+  );
+}
+
+export default function Navigation() {
+  return (
+    <Navbar className="d-flex flex-column bg-primary flex-fill rounded-3 overflow-hidden p-0">
+      <Container
+        fluid
+        className="d-flex justify-content-center bg-secondary py-3"
+      >
+        <NavLink to="/" className='text-white'>Word-Count-Analyzer</NavLink>
+      </Container>
+      <Nav className="d-flex flex-column w-100 my-2" id="MainNav">
+        <NavEntry to="/" icon="bi bi-bar-chart" text="DASHBOARD" />
+        <NavEntry to="/import" icon="bi bi-upload" text="IMPORT" />
+      </Nav>
+    </Navbar>
+  );
+}
